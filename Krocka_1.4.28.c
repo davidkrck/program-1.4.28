16bc31e8ffdf48127c1ad7b477ded2444b23c1b0
davidkrck <dkrockad@gmail.com>
2020-04-08 19:11:09 +0200

    pridany_vypis max,min

---
+++ Krocka_1.4.28.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void rozsah_hodnot(float *data, int l, float *min, float *max){
+    int i;
+    min = 0;
+    max = 0;
+
+    for(i = 0, i < l; i++){
+        if()
+        else
+
+    }
+}
+
+int main(){
+    int l = 8;
+    float data[] = (3,2,7,15,2,75,1,98);
+    float max, min;
+
+    printf("max :%f", max);
+    printf("min :%f", min);
+
+
+
+}
