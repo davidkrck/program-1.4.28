fa9f8ea1b59a3bd9f19a3afc574bb701e827141c
davidkrck <dkrockad@gmail.com>
2020-04-08 19:32:55 +0200

    pridané volanie funkcie, opraava čiarok a zátvoriek

--- Krocka_1.4.28.c
+++ Krocka_1.4.28.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
+void rozsah_hodnot(float *data, int l, float *min, float *max);
+
+int main(){
+    int l = 8;
+    float data[] = {3,2,7,15,2,75,1,98};
+    float max, min;
+
+    rozsah_hodnot(data, l, &max, &min);
+
+    printf("max :%f", max);
+    printf("min :%f", min);
+
+}
+
 void rozsah_hodnot(float *data, int l, float *min, float *max){
     int i;
     *min = data[0];
     *max = data[0];
 
-    for(i = 0, i < l; i++){
+    for(i = 0; i < l; i++){
         if(*min < data[i])
             *min = data[i];
         else if (*max > data[i])
             *max = data[i];    
     }
-}
-
-int main(){
-    int l = 8;
-    float data[] = (3,2,7,15,2,75,1,98);
-    float max, min;
-
-    printf("max :%f", max);
-    printf("min :%f", min);
-
-
-
 }
